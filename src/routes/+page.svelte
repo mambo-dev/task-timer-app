34d99445c60e3e3cc574f2c4b94cefa2b15b1c7b
mambo-dev <mambo.michael.22@gmail.com>
2024-01-24 22:06:07 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+
+  <div class="backdrop-blur-xl bg-white/30 max-w-5xl mt-10 mx-auto  min-h-[500px] rounded-md " >
+    <h1 class="text-3xl z-10 text-black font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+  
+  <style lang="postcss">
+    /* :global(html) {
+      background-color: theme(colors.blue.300) ;
+    } */
+  </style>
